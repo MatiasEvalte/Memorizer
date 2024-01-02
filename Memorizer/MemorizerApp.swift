623f7bdbafef27097fb97d5bd0725390b49a1895
Matias Borges Evalte <matias.evalte@zallpy.com>
2024-01-02 12:52:16 -0300

    Initial Commit

---
+++ Memorizer/MemorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizerApp.swift
+//  Memorizer
+//
+//  Created by Matias Evalte on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
