1249d165e6a701b1129709ab5064523f5184533e
Matias Borges Evalte <matias.evalte@zallpy.com>
2024-03-11 15:52:13 -0300

    Adicionado botões e suas lógicas para adicionar e remover os cards e criação de variáveis para separação de código.

--- Memorizer/ContentView.swift
+++ Memorizer/ContentView.swift
@@ -8,18 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        HStack {
-          CardView(isFaceUp: true)
-          CardView(isFaceUp: false)
-          CardView(isFaceUp: false)
-          CardView(isFaceUp: true)
-        }
-        .padding()
+  
+  enum Constants {
+    static let minValue: Int = 1
+    static let imageAdd: String = "rectangle.stack.badge.plus.fill"
+    static let imageRemove: String = "rectangle.stack.badge.minus.fill"
+  }
+  var cardArray: [String] = ["🤾‍♀️", "🏄‍♀️", "🏋️", "⛹️‍♀️", "🤾‍♀️", "🏄‍♀️", "🏋️", "⛹️‍♀️"]
+  @State var cardCount: Int = 4
+  
+  var body: some View {
+    VStack {
+      cards
+      HStack {
+        buttonAdd
+        Spacer()
+        buttonRemove
+      }.font(Font.title)
+    }.padding()
+  }
+}
+
+// MARK: - Internal methods
+
+extension ContentView {
+  var cards: some View {
+    HStack {
+      ForEach(0..<cardCount, id: \.self) { index in
+        CardView(content: cardArray[index],
+                 isFaceUp: false)
+      }
     }
+  }
+  
+  var buttonAdd: some View {
+    Button {
+      if cardCount < cardArray.count {
+        cardCount += 1
+      }
+    } label: {
+      Image(systemName: Constants.imageAdd)
+    }.foregroundColor(Color.red)
+      .imageScale(Image.Scale.large)
+  }
+  
+  var buttonRemove: some View {
+    Button {
+      if cardCount > Constants.minValue {
+        cardCount -= 1
+      }
+    } label: {
+      Image(systemName: Constants.imageRemove)
+    }.foregroundColor(Color.red)
+      .imageScale(Image.Scale.large)
+  }
 }
 
 struct CardView: View {
+  let content: String
   @State var isFaceUp: Bool
   
   var body: some View {
@@ -28,7 +74,7 @@ struct CardView: View {
       if isFaceUp {
         rectangle.foregroundColor(Color.yellow)
         rectangle.strokeBorder(lineWidth: 2)
-        Text("🙅🏼‍♂️🙅🏼‍♂️")
+        Text(content)
           .foregroundColor(Color.orange)
       } else {
         rectangle.foregroundColor(Color.red)
@@ -37,12 +83,12 @@ struct CardView: View {
     }.foregroundColor(.black)
       .onTapGesture {
         isFaceUp.toggle()
-    }
+      }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
